7593110aff5d50eabeda97d41b9d1a74a86de877
Hamsda <dennis.ehmig@googlemail.com>
2018-09-16 08:32:03 +0200

    split capture from tracker

--- ootrando_overworldmap_hamsda/scripts/init.lua
+++ ootrando_overworldmap_hamsda/scripts/init.lua
@@ -10,4 +10,5 @@ Tracker:AddLocations("locations/overworld.json")
 Tracker:AddLocations("locations/dungeons.json")
 
 Tracker:AddLayouts("layouts/tracker.json")
+Tracker:AddLayouts("layouts/capture.json")
 Tracker:AddLayouts("layouts/broadcast.json")
